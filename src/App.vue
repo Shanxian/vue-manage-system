d7c6e79aeb83448289a090c9fb7601c6620982e8
Shanxian Ji <735929039@qq.com>
2023-01-31 20:45:32 +0800

    added test code

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
-    <el-config-provider :locale="zhCn">
-        <router-view />
-    </el-config-provider>
+  <el-config-provider :locale="lang">
+    <router-view />
+  </el-config-provider>
 </template>
 
 <script setup lang="ts">
+import { reactive } from 'vue';
 import { ElConfigProvider } from 'element-plus';
 import zhCn from 'element-plus/es/locale/lang/zh-cn';
+import en from 'element-plus/es/locale/lang/en';
+
+const lang = reactive(localStorage.getItem('lang') === 'en' ? en : zhCn);
 </script>
+
 <style>
 @import './assets/css/main.css';
 @import './assets/css/color-dark.css';
